66c9061eecd437e978b60d23a1958c9832a67466
Andlie94 <andlie02174@stud.noroff.no>
2025-03-13 15:03:23 +0100

    fjernet noe

--- js/utils/storage.test.js
+++ js/utils/storage.test.js
@@ -2,26 +2,26 @@ import { describe, expect, it, beforeEach, vi } from "vitest";
 import { getUsername } from "./storage.js";
 
 beforeEach(() => {
-  globalThis.localStorage = {
+  window.localStorage = {
     getItem: vi.fn(),
     setItem: vi.fn(),
     removeItem: vi.fn(),
     clear: vi.fn(),
   };
 
-  Object.values(globalThis.localStorage).forEach((fn) => fn.mockReset());
+  Object.values(localStorage).forEach((fn) => fn.mockReset());
 });
 
 describe("getUsername", () => {
   it("return username from storage", () => {
-    globalThis.localStorage.getItem.mockReturnValue(
+    localStorage.getItem.mockReturnValue(
       JSON.stringify({ name: "Andre Strøm" }),
     );
     expect(getUsername()).toBe("Andre Strøm");
   });
 
   it("should return null if no user is stored", () => {
-    globalThis.localStorage.getItem.mockReturnValue(null);
+    localStorage.getItem.mockReturnValue(null);
     expect(getUsername()).toBe(null);
   });
 });
