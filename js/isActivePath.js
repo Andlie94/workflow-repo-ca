235b12b189f4bf85b12b0d4ebc4dc9c4c1f80b65
Andre Lier Strøm <160486912+Andlie94@users.noreply.github.com>
2025-03-13 11:21:29 +0100

    Delete js/isActivePath.js

--- js/isActivePath.js
+++
@@ -1,6 +0,0 @@
-export function isActivePath(currentPath, href) {
-  if (currentPath === "/" && (href === "/" || href === "/index.html")) {
-    return true;
-  }
-  return currentPath === href || currentPath.includes(href);
-}
